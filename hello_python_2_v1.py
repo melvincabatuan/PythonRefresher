dba71e8c497d46a73d18e45341f259eadb6befa7
Melvin Cabatuan <melvincabatuan@users.noreply.github.com>
2021-04-07 10:52:03 +0800

    Added #2

---
+++ hello_python_2_v1.py
@@ -0,0 +1,316 @@
+r'''
+              ___________________________________________________
+            /                                                    \`
+           |    _____________________________________________     |
+           |   |                                             |    |
+           |   |  >>> _                                      |    |
+           |   |                                             |    |
+           |   |             Python Refresher II             |    |
+           |   |                                             |    |
+           |   |                    for                      |    |
+           |   |                                             |    |
+           |   |                  Learners                   |    |
+           |   |                                             |    |
+           |   |                                             |    |
+           |   |                     by                      |    |
+           |   |                                             |    |
+           |   |                     MKC                     |    |
+           |   |_____________________________________________|    |
+           |                                                      |
+            \_____________________________________________________/
+                   \_______________________________________/
+                _______________________________________________
+             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_
+          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_
+       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_
+    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_
+ _-'.-.-.-.-.-. .---.-. .-----------------------------. .-.---. .---.-.-.-.`-_
+:-----------------------------------------------------------------------------:
+`---._.-----------------------------------------------------------------._.---'
+
+
+           ___   _      _              _    _
+          / _ \ | |__  (_)  ___   ___ | |_ (_)__   __ ___  ___
+         | | | || '_ \ | | / _ \ / __|| __|| |\ \ / // _ \/ __|
+         | |_| || |_) || ||  __/| (__ | |_ | | \ V /|  __/\__ \
+          \___/ |_.__/_/ | \___| \___| \__||_|  \_/  \___||___/
+                     |__/
+
+
+        • To practice conditionals and iteration as applied to
+          creating basic patterns.
+
+
+========================================================================
+Drill 1-1 Write a Python program that prints the following box pattern:
+(Use random and string module to generate random uppercase characters)
+========================================================================
+e.g.
+
+      KBNRRXHJTKRROAZKIFIGWXQOIWMAYNXQWNKKXRXEBLXBQTXGZS
+      ZRGUQSOTOUHHIQHKDHABJOIKGNSVCPTPKBDZAHKGQMZHJUGGKE
+      TBKHSGNMOHEJQABOSAIYRFPNUBPMIJGVFJXLPXZTAKIUUYPYST
+      HCLBYXTHETFWTRTRITCKNIFDODLIEZKQGADLNZPEVEMGLMPWAG
+      JMVBRZYQYDIVQFRUHJZQDESOWDTWRTZMJUBHSJOGFVEQCEAQOK
+      YGROYHBOTMRVSYBAITAQPEMGXRDDYOVMIDFPYRLEMVTUETTAVJ
+      ICGIEOAATRJHJSIPLATDBOHNXEBHJDXHHGLEUWBPPPSYNIVRCG
+      MCPDDFJHHJKOLTDWTCTLANKKFMERBHJTWPGMILKZWESITPDWXX
+      VMJLKJVSOECXQYBHLLSWOJONFQHJTXAPGFNFFGVNBPHAPXZFSG
+      PHQBCYXDGOBUBVMIEUFXNAWIMGTDYAOBJFNXRBETNHDFQYQUFI
+
+========================================================================
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-2 Write a Python program that prints the following hollow box
+pattern: (Use random & string module to generate random uppercase characters)
+========================================================================
+e.g.
+
+          XIOAFFUTMCHEYQKODLEAVLWREBFXTRTHCTRLLISJ
+          T                                      R
+          U                                      E
+          A                                      V
+          B                                      L
+          F                                      I
+          Y                                      L
+          W                                      T
+          E                                      V
+          IEFEJRRCWLZQRKUBJMXIOWHPKKOPVTPMTIMSRJGP
+
+========================================================================
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-3 Write a Python program that prints the following right triangle
+pattern: (Use random & string module to generate random uppercase characters)
+========================================================================
+e.g.
+
+  J
+  IK
+  FXY
+  VLFR
+  IWNNU
+  YMUAXP
+  IHGLFOH
+  AKLDHXKZ
+  BNMPEYXSK
+  QPPHJJKNSU
+========================================================================
+'''
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-4 Write a Python program that prints the following hollow right
+triangle pattern: (Use random & string module to generate random uppercase
+characters)
+========================================================================
+e.g.
+
+  R
+  YU
+  E N
+  Q  R
+  W   B
+  C    N
+  Z     P
+  W      U
+  Q       G
+  CJIJLNGRYT
+========================================================================
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-5 Write a Python program that prints the following inverted right
+triangle pattern:
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g.
+
+  RRJABDRNIO
+  UZKMYLPNF
+  YLPSPBSG
+  WPLEBML
+  FQYZYM
+  IVERP
+  BPIR
+  FEL
+  KQ
+  Y
+========================================================================
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-5 Write a Python program that prints the following inverted right
+hollow triangle pattern:
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g.
+
+  NUHAIFYPCU
+  V       A
+  Q      E
+  W     G
+  M    F
+  Y   D
+  C  U
+  B I
+  KF
+  P
+========================================================================
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-6 Write a Python program that prints the following right triangle
+pattern (right-side oriented). Use h variable to denote the height.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 10:
+         J
+        BC
+       IBO
+      NLXW
+     QZVJQ
+    HACRSK
+   QOBLCNF
+  GWGLXPBL
+ BJDUIITFB
+RLLTTENZEB
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-7 Write a Python program that prints the following hollow right
+triangle pattern (right-side oriented). Use h variable to denote the height.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 10:
+         F
+        PD
+       F P
+      G  W
+     M   N
+    T    J
+   N     W
+  V      F
+ R       L
+NKKEFGDMAU
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-8 Write a Python program that prints the following pyramid pattern
+Use h variable to denote the height.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 10:
+         N
+        CUO
+       HTNUY
+      AMNQHGE
+     SOPIKVVBN
+    OXXOHQSVMGH
+   TIAYEKBMCUUFT
+  VTHBWFJQRLSPVXR
+ VZWXDSAQGMPWJZWDL
+YGUNHILFRGVPCSJUUDE
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-9 Write a Python program that prints the following hollow pyramid
+pattern. Use h variable to denote the height.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 10:
+          U
+         D K
+        I   W
+       Y     Y
+      T       Z
+     G         B
+    B           A
+   C             E
+  I               O
+ Z                 B
+USQLKVWCMGCOCUDAVJOMU
+'''
+
+# YOUR CODE HERE!
+
+
+r'''
+========================================================================
+Drill 1-10 Write a Python program that prints the following diamond
+pattern. Use h variable to denote the height to center.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 5:
+     E
+    KHZ
+   BQFJM
+  MGXERZT
+ IFQKNKRJQ
+KLSMFFJNFNU
+ MOGJJJGUB
+  HOZACFQ
+   VBHCG
+    XSX
+     A
+'''
+
+# YOUR CODE HERE!
+
+
+
+r'''
+========================================================================
+Drill 1-11 Write a Python program that prints the following hollow diamond
+pattern. Use h variable to denote the height to center.
+(Use random & string module to generate random uppercase characters)
+========================================================================
+e.g. at h = 5:
+      R
+     T W
+    Y   G
+   T     W
+  V       R
+   E     J
+    T   G
+     V E
+      Y
+'''
+
+# YOUR CODE HERE!
